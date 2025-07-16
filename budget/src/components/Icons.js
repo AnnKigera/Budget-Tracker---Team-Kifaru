3534d310bfdb640cb91624df0862ad93ccc06c2e
veronicah <veronicah.mbuvi@strathmore.edu>
2025-07-16 17:42:51 +0300

    loaded tha applicati

---
+++ budget/src/components/Icons.js
@@ -0,0 +1,51 @@
+
+
+const iconClass = "w-5 h-5 inline-block align-middle";
+
+export const PlusCircle = () => (
+  <img
+    src="https://img.icons8.com/ios-filled/24/plus-math.png"
+    alt="Add"
+    className={iconClass}
+  />
+);
+
+export const Trash2 = () => (
+  <img
+    src="https://img.icons8.com/ios-filled/24/delete-forever.png"
+    alt="Delete"
+    className={iconClass}
+  />
+);
+
+export const DollarSign = () => (
+  <img
+    src="https://img.icons8.com/ios-filled/24/us-dollar-circled--v1.png"
+    alt="Dollar"
+    className={iconClass}
+  />
+);
+
+export const TrendingUp = () => (
+  <img
+    src="https://img.icons8.com/ios-filled/24/increase.png"
+    alt="Trending Up"
+    className={iconClass}
+  />
+);
+
+export const TrendingDown = () => (
+  <img
+    src="https://img.icons8.com/ios-filled/24/decrease.png"
+    alt="Trending Down"
+    className={iconClass}
+  />
+);
+
+export const Wallet = () => (
+  <img
+    src="https://img.icons8.com/ios-filled/24/wallet--v1.png"
+    alt="Wallet"
+    className={iconClass}
+  />
+);
